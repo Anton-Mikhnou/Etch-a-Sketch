aa4a8b6bc14108e39f94d6d5ab5051b81f29009a
a.mihnov <a.mihnov@anlax.org>
2023-09-16 11:36:48 +0300

    add chenge color

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
 let rows = 16;
 let column = 16;
 
+let gridDiv;
+
 const grid = document.querySelector('.display');
 for(let i = 0; i < rows*column; i++){
-    const gridDiv = document.createElement('div');
+    gridDiv = document.createElement('div');
     gridDiv.classList.add('item');
     grid.appendChild(gridDiv);
 }
 
-const itemGrid = document.querySelectorAll('.item')
-itemGrid.forEach ((items) => {
-    items.addEventListener('mousedown', (event) =>{
-        const item = event.target 
-        let letters = '0123456789ABCDEF';
-        let color = '#';
-        for (let i = 0; i < 6; i++){
-            color += letters[Math.floor(Math.random()*16)];
-        }
-        return color;
-    } )
+grid.addEventListener('click', (event) => {
+    const itemElem = event.target;
+    itemElem.classList.add('active')
+    console.log(event.target)
 })
+
